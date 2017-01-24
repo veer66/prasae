29c5c689ba36d76dddc25feb36a383f6e7cc7251
Vee Satayamas <vsatayamas@gmail.com>
2017-01-24 22:26:43 +0700

    update project info

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
-const gulp = require("gulp");
-const babel = require("gulp-babel");
-const mocha = require('gulp-mocha');
+const gulp = require("gulp")
+const babel = require("gulp-babel")
+const mocha = require('gulp-mocha')
 
 gulp.task("default", () =>
   gulp.src("src/*.js")
@@ -8,5 +8,7 @@ gulp.task("default", () =>
     .pipe(gulp.dest("dist")))
 
 gulp.task("test", () => 
-  gulp.src('test/test_*.js', {read: false})
-      .pipe(mocha({reporter: 'nyan'})))
+          gulp.src('test/test_*.js', {read: false})
+          .pipe(mocha({
+            reporter: 'nyan'            
+          })))
